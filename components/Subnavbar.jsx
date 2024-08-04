38d33599069659a89f4421ec2c502895172e95cb
iamshobh <iamshobhraj@gmail.com>
2024-08-04 20:19:43 +0530

    responsiveness

--- components/Subnavbar.jsx
+++ components/Subnavbar.jsx
@@ -3,7 +3,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export default  function Subnavbar() {  
     return(
         <>
-            <Tabs defaultValue="account" className="w-full">
+            <Tabs defaultValue="about" className="w-full">
               <TabsList>
                 <TabsTrigger value="about">About</TabsTrigger>
                 <TabsTrigger value="venue">Venue</TabsTrigger>
@@ -11,7 +11,6 @@ export default  function Subnavbar() {
                 <TabsTrigger value="date">Important Dates</TabsTrigger>
                 <TabsTrigger value="committee">Committee</TabsTrigger>
                 <TabsTrigger value="fees">Registration Fees (including GST)</TabsTrigger>
-
               </TabsList>
               <TabsContent value="about">
                 about component goes here
