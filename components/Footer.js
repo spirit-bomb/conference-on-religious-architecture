0e53b08d1960b656ca803fbeefbf5b40a08fe319
iamshobh <iamshobhraj@gmail.com>
2024-08-08 10:29:04 +0530

    new contact

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <p className="text-xl font-semibold antialiased">Contact Us </p><br />
     Department of Architecture and Planning, NIT Patna <br />
     Ashok Rajpath, Patna 800005, Bihar, India <br />
-    email: <a className='text-blue-500' href="mailto:icra@nitp.ac.in">icra@nitp.ac.in</a></div>
+    email: <a className='text-blue-500' href="mailto:icra2024@nitp.ac.in">icra2024@nitp.ac.in</a></div>
   )
 }
 
